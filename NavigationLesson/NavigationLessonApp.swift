2783356b64b0fcba0a31fefb51e73691e67505e1
Nicole Bond <nicbond22@gmail.com>
2024-06-24 10:23:39 -0400

    Initial Commit

---
+++ NavigationLesson/NavigationLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationLessonApp.swift
+//  NavigationLesson
+//
+//  Created by Scholar on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
